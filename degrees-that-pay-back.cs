8458cc1953c91e58dd65ec550e9ae62f2f46b068
Kalea Bright <brightkk@scottsvalleyusd.org>
2019-02-08 16:21:34 +0000

    first commit

---
+++ degrees-that-pay-back.cs
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" class="__meteor-css__" href="/fd5b3fdc396944205792bb831a4e42330af648c6.css?meteor_css_resource=true">
+<meta charset="utf-8">
+  <title>Miguel Rocha</title>
+  <!--<link rel="icon" 
+        type="image/png" 
+       href="SciTechAlogo_v16_square_noArtboard_favicon.png"> -->
+  <meta name="description" content="Miguel Rocha's app">
+  <meta name="viewport" content="user-scalable=no, initial-scale=1, minimal-ui, maximum-scale=1, minimum-scale=1" />
+
+</head>
+<body>
+
+
+
+  <script type="text/javascript">__meteor_runtime_config__ = JSON.parse(decodeURIComponent("%7B%22meteorRelease%22%3A%22METEOR%401.5.2.2%22%2C%22meteorEnv%22%3A%7B%22NODE_ENV%22%3A%22production%22%2C%22TEST_METADATA%22%3A%22%7B%7D%22%7D%2C%22PUBLIC_SETTINGS%22%3A%7B%7D%2C%22ROOT_URL%22%3A%22https%3A%2F%2Fmrocha.org%22%2C%22ROOT_URL_PATH_PREFIX%22%3A%22%22%2C%22appId%22%3A%221oanogjktfn21ptlgr9%22%2C%22autoupdateVersion%22%3A%22c1f16568df2d16021f7c1d2c235bf7de8985e16b%22%2C%22autoupdateVersionRefreshable%22%3A%22d3c8a60e9f4b547af3f5b75c789ef296cf7d01bf%22%2C%22autoupdateVersionCordova%22%3A%22none%22%7D"))</script>
+
+  <script type="text/javascript" src="/b00d1bf7d4c6a44850a4d4df5fce55050db0066e.js?meteor_js_resource=true"></script>
+
+
+</body>
+</html>
